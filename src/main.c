0bbbb0a13a9484cef3066c2db1a35d3d63d2f4d3
Giulio Zausa <mail.zausa.giulio@gmail.com>
2022-12-06 23:29:18 +0100

    Audio example

--- src/main.c
+++ src/main.c
@@ -1,32 +1,96 @@
 #include <stdio.h>
+#include <math.h>
 
 #include "config.h"
-#include "devices/audio_pio.h"
+#include "pico/audio_i2s.h"
+#include "pico/stdlib.h"
+
+#define SINE_WAVE_TABLE_LEN 2048
+#define SAMPLES_PER_BUFFER 256
+
+static int16_t sine_wave_table[SINE_WAVE_TABLE_LEN];
+
+struct audio_buffer_pool *init_audio() {
+    static audio_format_t audio_format = {
+            .format = AUDIO_BUFFER_FORMAT_PCM_S16,
+            .sample_freq = 24000,
+            .channel_count = 1,
+    };
+
+    static struct audio_buffer_format producer_format = {
+            .format = &audio_format,
+            .sample_stride = 2
+    };
+
+    struct audio_buffer_pool *producer_pool = audio_new_producer_pool(&producer_format, 3,
+                                                                      SAMPLES_PER_BUFFER); // todo correct size
+    bool __unused ok;
+    const struct audio_format *output_format;
+    struct audio_i2s_config config = {
+            .data_pin = PICO_AUDIO_I2S_DATA_PIN,
+            .clock_pin_base = PICO_AUDIO_I2S_CLOCK_PIN_BASE,
+            .dma_channel = 0,
+            .pio_sm = 0,
+    };
+
+    output_format = audio_i2s_setup(&audio_format, &config);
+    if (!output_format) {
+        panic("PicoAudio: Unable to open audio device.\n");
+    }
+
+    ok = audio_i2s_connect(producer_pool);
+    assert(ok);
+    audio_i2s_set_enabled(true);
+    return producer_pool;
+}
 
 int main() {
-  pio_init();
+  stdio_init_all();
 
-  gpio_init(PIN_B1);
-  gpio_init(PIN_B2);
-  gpio_pull_up(PIN_B1);
-  gpio_pull_up(PIN_B2);
-  gpio_set_dir(PIN_B1, GPIO_IN);
-  gpio_set_dir(PIN_B2, GPIO_IN);
+  sleep_ms(4000);
 
-  int32_t *samples = (int32_t *)data_treating(sine_wave_table, 256);
-  int32_t *samples1 = NULL;
-
-  while (true) {
-    bool b1Value = gpio_get(PIN_B1);
-    bool b2Value = gpio_get(PIN_B2);
-
-    // for (int i = 20; i < 100; i = i + 20) {
-    //   sleep_ms(1000);
-    //   samples1 = (int32_t *)Volume_32((int16_t *)samples, 256, i);
-    //   Happy_birthday_out(samples1);
-    //   free_32(samples1);
-    // }
+  for (int i = 0; i < SINE_WAVE_TABLE_LEN; i++) {
+      sine_wave_table[i] = 32767 * cosf(i * 2 * (float) (M_PI / SINE_WAVE_TABLE_LEN));
   }
+
+  struct audio_buffer_pool *ap = init_audio();
+  uint32_t step = 0x200000;
+  uint32_t pos = 0;
+  uint32_t pos_max = 0x10000 * SINE_WAVE_TABLE_LEN;
+  uint vol = 128;
+  while (true) {
+      int c = getchar_timeout_us(0);
+      if (c >= 0) {
+          if (c == '-' && vol) vol -= 4;
+          if ((c == '=' || c == '+') && vol < 255) vol += 4;
+          if (c == '[' && step > 0x10000) step -= 0x10000;
+          if (c == ']' && step < (SINE_WAVE_TABLE_LEN / 16) * 0x20000) step += 0x10000;
+          if (c == 'q') break;
+          printf("vol = %d, step = %d      \r", vol, step >> 16);
+      }
+      struct audio_buffer *buffer = take_audio_buffer(ap, true);
+      int16_t *samples = (int16_t *) buffer->buffer->bytes;
+      for (uint i = 0; i < buffer->max_sample_count; i++) {
+          samples[i] = (vol * sine_wave_table[pos >> 16u]) >> 8u;
+          pos += step;
+          if (pos >= pos_max) pos -= pos_max;
+      }
+      buffer->sample_count = buffer->max_sample_count;
+      give_audio_buffer(ap, buffer);
+  }
+  puts("\n");
+
+  // gpio_init(PIN_B1);
+  // gpio_init(PIN_B2);
+  // gpio_pull_up(PIN_B1);
+  // gpio_pull_up(PIN_B2);
+  // gpio_set_dir(PIN_B1, GPIO_IN);
+  // gpio_set_dir(PIN_B2, GPIO_IN);
+
+  // while (true) {
+  //   bool b1Value = gpio_get(PIN_B1);
+  //   bool b2Value = gpio_get(PIN_B2);
+  // }
 
   return 0;
 }
